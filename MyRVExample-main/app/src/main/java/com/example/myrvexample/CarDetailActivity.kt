dd3de0078f9c564f79ba2000ccbdcb57b877153d
ykptn <ykaptan2121@gmail.com>
2024-11-08 22:37:02 +0300

    Final Version

--- MyRVExample-main/app/src/main/java/com/example/myrvexample/CarDetailActivity.kt
+++ MyRVExample-main/app/src/main/java/com/example/myrvexample/CarDetailActivity.kt
@@ -29,10 +29,34 @@ class CarDetailActivity : AppCompatActivity() {
         binding.specsView.text = carSpecification
         binding.performanceView.text = carPerformance
 
+        if (carName != null) {
+            binding.carImageView.setImageResource(getCarImageResource(carName))
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun getCarImageResource(carModel: String): Int {
+        return when (carModel) {
+            "i10" -> R.drawable.car_i10
+            "i20" -> R.drawable.car_i20
+            "Elantra" -> R.drawable.car_elantra
+            "i20 N" -> R.drawable.car_i20_n
+            "Bayon" -> R.drawable.car_bayon
+            "Kona" -> R.drawable.car_kona
+            "Kona Hybrid" -> R.drawable.car_kona_hybrid
+            "Tucson" -> R.drawable.car_tucson
+            "Tucson Hybrid" -> R.drawable.car_tucson_hybrid
+            "Santa Fe Hybrid" -> R.drawable.car_santa_fe_hybrid
+            "Kona Electric" -> R.drawable.car_kona_electric
+            "IONIQ 5" -> R.drawable.car_ioniq_5
+            "IONIQ 6" -> R.drawable.car_ioniq_6
+            "STARIA" -> R.drawable.car_staria
+            else -> R.drawable.hyundai_logo
         }
     }
 }
