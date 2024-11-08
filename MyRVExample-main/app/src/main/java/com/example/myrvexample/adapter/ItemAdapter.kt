104ad171ec97d85f63cbce11d3889edefa6145df
ykptn <ykaptan2121@gmail.com>
2024-11-08 12:59:26 +0300

    Update 1

--- MyRVExample-main/app/src/main/java/com/example/myrvexample/adapter/ItemAdapter.kt
+++ MyRVExample-main/app/src/main/java/com/example/myrvexample/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myrvexample.R
@@ -17,6 +18,7 @@ class ItemAdapter(private val data: List<Car>): RecyclerView.Adapter<ItemAdapter
 
         val nameView: TextView = view.findViewById(R.id.nameTV)
         val performanceView: TextView = view.findViewById(R.id.performanceTV)
+        val carImageView: ImageView = view.findViewById(R.id.carImageView)
         init {
             view.setOnClickListener{
                 val context = itemView.context
@@ -27,10 +29,32 @@ class ItemAdapter(private val data: List<Car>): RecyclerView.Adapter<ItemAdapter
                 context.startActivity(showCarIntent)
             }
         }
-        fun bind(car:Car){
+        fun bind(car: Car) {
             this.car = car
             nameView.text = car.name
             performanceView.text = car.performance
+            val carImageRes = getCarImageResource(car.name)
+            carImageView.setImageResource(carImageRes)
+        }
+
+        private fun getCarImageResource(carModel: String): Int {
+            return when (carModel) {
+                "i10" -> R.drawable.car_i10
+                "i20" -> R.drawable.car_i20
+                "Elantra" -> R.drawable.car_elantra
+                "i20 N" -> R.drawable.car_i20_n
+                "Bayon" -> R.drawable.car_bayon
+                "Kona" -> R.drawable.car_kona
+                "Kona Hybrid" -> R.drawable.car_kona_hybrid
+                "Tucson" -> R.drawable.car_tucson
+                "Tucson Hybrid" -> R.drawable.car_tucson_hybrid
+                "Santa Fe Hybrid" -> R.drawable.car_santa_fe_hybrid
+                "Kona Electric" -> R.drawable.car_kona_electric
+                "IONIQ 5" -> R.drawable.car_ioniq_5
+                "IONIQ 6" -> R.drawable.car_ioniq_6
+                "STARIA" -> R.drawable.car_staria
+                else -> R.drawable.hyundai_logo
+            }
         }
     }
 
